b0caffb0c56ff0f9e482e7e7696144fcc1a55918
杨冠超 <guanchao.ygc@alibaba-inc.com>
2018-05-19 21:22:16 +0800

    test cases

--- tracer-core/src/test/java/com/alipay/common/tracer/core/appender/self/SelfLogTest.java
+++ tracer-core/src/test/java/com/alipay/common/tracer/core/appender/self/SelfLogTest.java
@@ -17,6 +17,7 @@
 package com.alipay.common.tracer.core.appender.self;
 
 import com.alipay.common.tracer.core.appender.TracerLogRootDeamon;
+import com.alipay.common.tracer.core.appender.manager.AsyncCommonAppenderManager;
 import com.alipay.common.tracer.core.base.AbstractTestBase;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -24,6 +25,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
@@ -43,6 +45,7 @@ public class SelfLogTest extends AbstractTestBase {
         if (file.exists()) {
             FileUtils.writeStringToFile(file, "");
         }
+        reflectSelfLog();
     }
 
     @After
@@ -59,9 +62,8 @@ public class SelfLogTest extends AbstractTestBase {
      */
     @Test
     public void testErrorForLogE() throws Exception {
-        System.err.println("testErrorForLogE");
         SelfLog.error("Error info", new RuntimeException("RunTimeException"));
-        Thread.sleep(8000);
+        Thread.sleep(4000);
 
         List<String> logs = FileUtils.readLines(new File(TracerLogRootDeamon.LOG_FILE_DIR
                                                          + File.separator + SelfLog.SELF_LOG_FILE));
@@ -73,9 +75,8 @@ public class SelfLogTest extends AbstractTestBase {
      */
     @Test
     public void testErrorWithTraceIdForLogE() throws Exception {
-        System.err.println("testErrorForLogE");
         SelfLog.errorWithTraceId("error Info ", "traceid");
-        Thread.sleep(8000);
+        Thread.sleep(4000);
         List<String> logs = FileUtils.readLines(new File(TracerLogRootDeamon.LOG_FILE_DIR
                                                          + File.separator + SelfLog.SELF_LOG_FILE));
         assertTrue(logs.toString(), logs.size() > 0);
@@ -89,7 +90,7 @@ public class SelfLogTest extends AbstractTestBase {
         SelfLog.error("Error info");
         SelfLog.error("Error", new RuntimeException("error"));
 
-        Thread.sleep(8000);
+        Thread.sleep(4000);
 
         List<String> logs = FileUtils.readLines(new File(TracerLogRootDeamon.LOG_FILE_DIR
                                                          + File.separator + SelfLog.SELF_LOG_FILE));
@@ -98,5 +99,14 @@ public class SelfLogTest extends AbstractTestBase {
             stringBuilder.append("\n" + logs.get(i));
         }
         assertTrue(stringBuilder.toString(), logs.size() > 0);
+    }
+
+    private static void reflectSelfLog() throws NoSuchFieldException, IllegalAccessException {
+        //clear
+        Field fieldAsync = SelfLog.class.getDeclaredField("selfLogAppenderManager");
+        fieldAsync.setAccessible(true);
+        AsyncCommonAppenderManager selfLogAppenderManager = new AsyncCommonAppenderManager(1024, SelfLog.SELF_LOG_FILE);
+        selfLogAppenderManager.start("SelfLogAppender");
+        fieldAsync.set(null, selfLogAppenderManager);
     }
 }
