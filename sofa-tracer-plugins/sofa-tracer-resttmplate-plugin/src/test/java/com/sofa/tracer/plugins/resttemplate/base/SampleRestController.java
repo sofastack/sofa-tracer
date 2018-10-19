6491f75ae5554e67b9e56b2a3d301d0b6b46388b
卫恒 <guolei.sgl@antfin.com>
2018-10-19 15:32:52 +0800

    test fix

--- sofa-tracer-plugins/sofa-tracer-resttmplate-plugin/src/test/java/com/sofa/tracer/plugins/resttemplate/base/SampleRestController.java
+++ sofa-tracer-plugins/sofa-tracer-resttmplate-plugin/src/test/java/com/sofa/tracer/plugins/resttemplate/base/SampleRestController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * SampleRestController
  *
