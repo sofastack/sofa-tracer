9347f692c1adc6eeba1ec9d1217cb29adc549b11
QilongZhang <qilong.zql@alibaba-inc.com>
2018-10-24 00:08:49 +0800

    code format

--- tracer-sofa-boot-starter/src/main/java/com/alipay/sofa/tracer/boot/listener/SofaTracerConfigurationListener.java
+++ tracer-sofa-boot-starter/src/main/java/com/alipay/sofa/tracer/boot/listener/SofaTracerConfigurationListener.java
@@ -1,6 +1,18 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2018 All Rights Reserved.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alipay.sofa.tracer.boot.listener;
 
@@ -22,9 +34,12 @@ import org.springframework.validation.BindException;
  * @author qilong.zql
  * @since 2.2.2
  */
-public class SofaTracerConfigurationListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent>, PriorityOrdered {
+public class SofaTracerConfigurationListener
+                                            implements
+                                            ApplicationListener<ApplicationEnvironmentPreparedEvent>,
+                                            PriorityOrdered {
 
-    public final static String SOFA_TRACER_CONFIGURATION_PREFIX="com.alipay.sofa.tracer";
+    public final static String SOFA_TRACER_CONFIGURATION_PREFIX = "com.alipay.sofa.tracer";
 
     @Override
     public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
@@ -32,42 +47,42 @@ public class SofaTracerConfigurationListener implements ApplicationListener<Appl
 
         // check spring.application.name
         String applicationName = environment
-                .getProperty(SofaTracerConfiguration.TRACER_APPNAME_KEY);
+            .getProperty(SofaTracerConfiguration.TRACER_APPNAME_KEY);
         Assert.isTrue(!StringUtils.isBlank(applicationName),
-                SofaTracerConfiguration.TRACER_APPNAME_KEY + " must be configured!");
+            SofaTracerConfiguration.TRACER_APPNAME_KEY + " must be configured!");
 
         SofaTracerProperties tempTarget = new SofaTracerProperties();
-        PropertiesConfigurationFactory<SofaTracerProperties> binder = new PropertiesConfigurationFactory<SofaTracerProperties>(tempTarget);
+        PropertiesConfigurationFactory<SofaTracerProperties> binder = new PropertiesConfigurationFactory<SofaTracerProperties>(
+            tempTarget);
         binder.setTargetName(SOFA_TRACER_CONFIGURATION_PREFIX);
         binder.setConversionService(new DefaultConversionService());
         binder.setPropertySources(environment.getPropertySources());
         try {
             binder.bindPropertiesToTarget();
-        }
-        catch (BindException ex) {
+        } catch (BindException ex) {
             throw new IllegalStateException("Cannot bind to SofaTracerProperties", ex);
         }
 
         //properties convert to tracer
         SofaTracerConfiguration.setProperty(
-                SofaTracerConfiguration.DISABLE_MIDDLEWARE_DIGEST_LOG_KEY,
-                tempTarget.getDisableDigestLog());
+            SofaTracerConfiguration.DISABLE_MIDDLEWARE_DIGEST_LOG_KEY,
+            tempTarget.getDisableDigestLog());
         SofaTracerConfiguration.setProperty(SofaTracerConfiguration.DISABLE_DIGEST_LOG_KEY,
-                tempTarget.getDisableConfiguration());
+            tempTarget.getDisableConfiguration());
         SofaTracerConfiguration.setProperty(SofaTracerConfiguration.TRACER_GLOBAL_ROLLING_KEY,
-                tempTarget.getTracerGlobalRollingPolicy());
+            tempTarget.getTracerGlobalRollingPolicy());
         SofaTracerConfiguration.setProperty(SofaTracerConfiguration.TRACER_GLOBAL_LOG_RESERVE_DAY,
-                tempTarget.getTracerGlobalLogReserveDay());
+            tempTarget.getTracerGlobalLogReserveDay());
         //stat log interval
         SofaTracerConfiguration.setProperty(SofaTracerConfiguration.STAT_LOG_INTERVAL,
-                tempTarget.getStatLogInterval());
+            tempTarget.getStatLogInterval());
         //baggage length
         SofaTracerConfiguration.setProperty(
-                SofaTracerConfiguration.TRACER_PENETRATE_ATTRIBUTE_MAX_LENGTH,
-                tempTarget.getBaggageMaxLength());
+            SofaTracerConfiguration.TRACER_PENETRATE_ATTRIBUTE_MAX_LENGTH,
+            tempTarget.getBaggageMaxLength());
         SofaTracerConfiguration.setProperty(
-                SofaTracerConfiguration.TRACER_SYSTEM_PENETRATE_ATTRIBUTE_MAX_LENGTH,
-                tempTarget.getBaggageMaxLength());
+            SofaTracerConfiguration.TRACER_SYSTEM_PENETRATE_ATTRIBUTE_MAX_LENGTH,
+            tempTarget.getBaggageMaxLength());
     }
 
     @Override
