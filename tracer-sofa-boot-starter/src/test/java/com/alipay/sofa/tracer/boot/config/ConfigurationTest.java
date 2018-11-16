a915ac4cbcbef65a9485c606b36a1b962b810a3b
QilongZhang <qilong.zql@alibaba-inc.com>
2018-11-16 18:53:32 +0800

    for test

--- tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/config/ConfigurationTest.java
+++ tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/config/ConfigurationTest.java
@@ -82,6 +82,8 @@ public class ConfigurationTest {
                                    + File.separator + "tracelog");
         File configDir = new File(System.getProperty("user.dir") + File.separator + "logs"
                                   + File.separator + "tracelog");
+        System.out.println(System.getProperty("logging.path"));
+        System.out.println(System.getProperty("user.dir"));
         Assert.assertFalse(defaultDir.exists());
         Assert.assertTrue(configDir.exists());
     }
