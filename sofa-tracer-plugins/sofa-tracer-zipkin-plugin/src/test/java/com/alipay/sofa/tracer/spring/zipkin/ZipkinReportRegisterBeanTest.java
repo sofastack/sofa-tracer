e5039eba2ad96039e2fcd5a7839ec4b5a6a7337c
卫恒 <guolei.sgl@antfin.com>
2018-11-21 19:16:01 +0800

    code format

--- sofa-tracer-plugins/sofa-tracer-zipkin-plugin/src/test/java/com/alipay/sofa/tracer/spring/zipkin/ZipkinReportRegisterBeanTest.java
+++ sofa-tracer-plugins/sofa-tracer-zipkin-plugin/src/test/java/com/alipay/sofa/tracer/spring/zipkin/ZipkinReportRegisterBeanTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.tracer.spring.zipkin;
 
 import com.alipay.common.tracer.core.listener.SpanReportListenerHolder;
@@ -14,16 +30,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author: guolei.sgl
  * @since: 18/11/21 下午5:54
  **/
-public class ZipkinReportRegisterBeanTest{
+public class ZipkinReportRegisterBeanTest {
 
-    private ClassPathXmlApplicationContext applicationContext ;
+    private ClassPathXmlApplicationContext applicationContext;
 
     @Before
-    public void init(){
+    public void init() {
         applicationContext = new ClassPathXmlApplicationContext("spring-bean.xml");
     }
+
     @Test
-    public void testAfterPropertiesSet(){
+    public void testAfterPropertiesSet() {
         Object zipkinReportRegisterBean = applicationContext.getBean("zipkinReportRegisterBean");
         Assert.assertTrue(zipkinReportRegisterBean instanceof ZipkinReportRegisterBean);
         Assert.assertTrue(SpanReportListenerHolder.getSpanReportListenersHolder().size() > 0);
