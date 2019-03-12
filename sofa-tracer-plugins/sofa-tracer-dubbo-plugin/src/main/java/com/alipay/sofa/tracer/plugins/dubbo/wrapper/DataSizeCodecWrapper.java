4643b3a071ec35811821ccd78001315dfe64731f
卫恒 <guolei.sgl@antfin.com>
2019-03-12 21:37:52 +0800

    delete import

--- sofa-tracer-plugins/sofa-tracer-dubbo-plugin/src/main/java/com/alipay/sofa/tracer/plugins/dubbo/wrapper/DataSizeCodecWrapper.java
+++ sofa-tracer-plugins/sofa-tracer-dubbo-plugin/src/main/java/com/alipay/sofa/tracer/plugins/dubbo/wrapper/DataSizeCodecWrapper.java
@@ -23,12 +23,9 @@ import org.apache.dubbo.remoting.Codec2;
 import org.apache.dubbo.remoting.buffer.ChannelBuffer;
 import org.apache.dubbo.remoting.exchange.Request;
 import org.apache.dubbo.remoting.exchange.Response;
-import org.apache.dubbo.remoting.exchange.support.MultiMessage;
 import org.apache.dubbo.rpc.RpcInvocation;
 import org.apache.dubbo.rpc.RpcResult;
-
 import java.io.IOException;
-import java.util.Collection;
 
 /**
  * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/2/26 7:46 PM
