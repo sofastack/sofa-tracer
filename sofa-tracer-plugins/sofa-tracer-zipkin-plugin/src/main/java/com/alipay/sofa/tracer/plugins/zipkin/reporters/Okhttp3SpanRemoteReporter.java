cdf6a654d89b932fb2af9ab3aad89b32009a7d58
卫恒 <guolei.sgl@antfin.com>
2019-03-13 20:57:55 +0800

    code format

--- sofa-tracer-plugins/sofa-tracer-zipkin-plugin/src/main/java/com/alipay/sofa/tracer/plugins/zipkin/reporters/Okhttp3SpanRemoteReporter.java
+++ sofa-tracer-plugins/sofa-tracer-zipkin-plugin/src/main/java/com/alipay/sofa/tracer/plugins/zipkin/reporters/Okhttp3SpanRemoteReporter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.tracer.plugins.zipkin.reporters;
 
 import com.alipay.common.tracer.core.listener.SpanReportListener;
@@ -17,13 +33,13 @@ import java.io.IOException;
  **/
 public class Okhttp3SpanRemoteReporter implements SpanReportListener, Flushable, Closeable {
 
-    private OkHttpSender okHttpSender;
-    private ZipkinV2SpanAdapter zipkinV2SpanAdapter;
-    private final OkHttpSender sender;
+    private OkHttpSender              okHttpSender;
+    private ZipkinV2SpanAdapter       zipkinV2SpanAdapter;
+    private final OkHttpSender        sender;
 
-    private final AsyncReporter<Span>      delegate;
+    private final AsyncReporter<Span> delegate;
 
-    public Okhttp3SpanRemoteReporter(){
+    public Okhttp3SpanRemoteReporter() {
         this.zipkinV2SpanAdapter = new ZipkinV2SpanAdapter();
         this.sender = OkHttpSender.create("htp://localhost:9411/api/v1/spans");
         this.delegate = AsyncReporter.create(sender);
