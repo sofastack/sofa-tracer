13bbb3511a75a0c17a25f378bcd49e6644bfe4c5
卫恒 <guolei.sgl@antfin.com>
2019-08-09 22:14:08 +0800

    cheery pick resttemplate

---
+++ tracer-sofa-boot-starter/src/main/java/com/alipay/sofa/tracer/boot/resttemplate/CustomRestTemplateCustomizer.java
@@ -0,0 +1,8 @@
+package com.alipay.sofa.tracer.boot.resttemplate;
+
+/**
+ * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/8/9 10:12 PM
+ * @since:
+ **/
+public class CustomRestTemplateCustomizer {
+}
