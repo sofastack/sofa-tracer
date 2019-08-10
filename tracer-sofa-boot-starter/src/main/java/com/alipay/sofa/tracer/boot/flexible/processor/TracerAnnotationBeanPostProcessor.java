285fc21ed8179e9523e106909fdf041a11511eed
卫恒 <guolei.sgl@antfin.com>
2019-08-10 16:37:24 +0800

    all for 3.x

---
+++ tracer-sofa-boot-starter/src/main/java/com/alipay/sofa/tracer/boot/flexible/processor/TracerAnnotationBeanPostProcessor.java
@@ -0,0 +1,8 @@
+package com.alipay.sofa.tracer.boot.flexible.processor;
+
+/**
+ * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/8/10 11:51 AM
+ * @since:
+ **/
+public class TracerAnnotationBeanPostProcessor {
+}
---
+++ tracer-sofa-boot-starter/src/main/java/com/alipay/sofa/tracer/boot/test/SofaTracerAspect.java
@@ -0,0 +1,8 @@
+package com.alipay.sofa.tracer.boot.test;
+
+/**
+ * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/8/10 12:17 AM
+ * @since:
+ **/
+public class SofaTracerAspect {
+}
