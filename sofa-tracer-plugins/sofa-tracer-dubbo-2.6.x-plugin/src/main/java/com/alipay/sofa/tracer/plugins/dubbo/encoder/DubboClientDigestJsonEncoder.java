1c6a457406552153f88503e70d3977ce000f4300
卫恒 <guolei.sgl@antfin.com>
2019-09-02 14:59:26 +0800

    support dubbo 2.6.x

---
+++ sofa-tracer-plugins/sofa-tracer-dubbo-2.6.x-plugin/src/main/java/com/alipay/sofa/tracer/plugins/dubbo/encoder/DubboClientDigestJsonEncoder.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.tracer.plugins.dubbo.encoder;
+
+import com.alipay.common.tracer.core.appender.builder.JsonStringBuilder;
+import com.alipay.common.tracer.core.appender.self.Timestamp;
+import com.alipay.common.tracer.core.context.span.SofaTracerSpanContext;
+import com.alipay.common.tracer.core.middleware.parent.AbstractDigestSpanEncoder;
+import com.alipay.common.tracer.core.span.CommonSpanTags;
+import com.alipay.common.tracer.core.span.SofaTracerSpan;
+import com.alipay.common.tracer.core.utils.StringUtils;
+import com.alipay.sofa.tracer.plugins.dubbo.constants.AttachmentKeyConstants;
+import io.opentracing.tag.Tags;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/2/26 3:56 PM
+ * @since:
+ **/
+public class DubboClientDigestJsonEncoder extends AbstractDigestSpanEncoder {
+
+    @Override
+    public String encode(SofaTracerSpan sofaTracerSpan) throws IOException {
+
+        JsonStringBuilder data = new JsonStringBuilder();
+        //span end time
+        data.appendBegin(CommonSpanTags.TIME, Timestamp.format(sofaTracerSpan.getEndTime()));
+
+        Map<String, String> tagStr = sofaTracerSpan.getTagsWithStr();
+        Map<String, Number> tagNum = sofaTracerSpan.getTagsWithNumber();
+        SofaTracerSpanContext context = sofaTracerSpan.getSofaTracerSpanContext();
+
+        // TraceId
+        data.append(CommonSpanTags.TRACE_ID, context.getTraceId());
+        // SpanId
+        data.append(CommonSpanTags.SPAN_ID, context.getSpanId());
+        // Span Type
+        data.append(Tags.SPAN_KIND.getKey(), tagStr.get(Tags.SPAN_KIND.getKey()));
+        // app name
+        data.append(CommonSpanTags.LOCAL_APP, tagStr.get(CommonSpanTags.LOCAL_APP));
+        // protocol
+        data.append(CommonSpanTags.PROTOCOL, tagStr.get(CommonSpanTags.PROTOCOL));
+        // serviceName
+        data.append(CommonSpanTags.SERVICE, tagStr.get(CommonSpanTags.SERVICE));
+        // method
+        data.append(CommonSpanTags.METHOD, tagStr.get(CommonSpanTags.METHOD));
+        //invoke type
+        data.append(CommonSpanTags.INVOKE_TYPE, tagStr.get(CommonSpanTags.INVOKE_TYPE));
+        //target ip
+        data.append(CommonSpanTags.REMOTE_HOST, tagStr.get(CommonSpanTags.REMOTE_HOST));
+        //target port
+        data.append(CommonSpanTags.REMOTE_PORT, tagStr.get(CommonSpanTags.REMOTE_PORT));
+        //local ip
+        data.append(CommonSpanTags.LOCAL_HOST, tagStr.get(CommonSpanTags.LOCAL_HOST));
+        //request serialize time
+        data.append(CommonSpanTags.CLIENT_SERIALIZE_TIME,
+            tagNum.get(AttachmentKeyConstants.CLIENT_SERIALIZE_TIME));
+        //response deserialize time
+        data.append(CommonSpanTags.CLIENT_DESERIALIZE_TIME,
+            tagNum.get(AttachmentKeyConstants.CLIENT_DESERIALIZE_TIME));
+        //Request Body bytes length
+        Number reqSizeNum = tagNum.get(AttachmentKeyConstants.CLIENT_SERIALIZE_SIZE);
+        data.append(CommonSpanTags.REQ_SIZE, reqSizeNum == null ? 0 : reqSizeNum.longValue());
+        //Response Body bytes length
+        Number respSizeNum = tagNum.get(AttachmentKeyConstants.CLIENT_DESERIALIZE_SIZE);
+        data.append(CommonSpanTags.RESP_SIZE, respSizeNum == null ? 0 : respSizeNum.longValue());
+
+        //Http status code
+        data.append(CommonSpanTags.RESULT_CODE, tagStr.get(CommonSpanTags.RESULT_CODE));
+        //error message
+        if (StringUtils.isNotBlank(tagStr.get(Tags.ERROR.getKey()))) {
+            data.append(Tags.ERROR.getKey(), tagStr.get(Tags.ERROR.getKey()));
+        }
+        //thread name
+        data.append(CommonSpanTags.CURRENT_THREAD_NAME,
+            tagStr.get(CommonSpanTags.CURRENT_THREAD_NAME));
+        //time-consuming ms
+        data.append(CommonSpanTags.TIME_COST_MILLISECONDS,
+            (sofaTracerSpan.getEndTime() - sofaTracerSpan.getStartTime()));
+        this.appendBaggage(data, context);
+        return data.toString();
+    }
+
+    private void appendBaggage(JsonStringBuilder jsonStringBuilder,
+                               SofaTracerSpanContext sofaTracerSpanContext) {
+        //baggage
+        jsonStringBuilder.appendEnd(CommonSpanTags.BAGGAGE,
+            baggageSerialized(sofaTracerSpanContext));
+    }
+
+}
---
+++ sofa-tracer-plugins/sofa-tracer-dubbo-2.6.x-plugin/src/main/java/com/alipay/sofa/tracer/plugins/dubbo/encoder/DubboServerDigestJsonEncoder.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.tracer.plugins.dubbo.encoder;
+
+import com.alipay.common.tracer.core.appender.builder.JsonStringBuilder;
+import com.alipay.common.tracer.core.appender.self.Timestamp;
+import com.alipay.common.tracer.core.context.span.SofaTracerSpanContext;
+import com.alipay.common.tracer.core.middleware.parent.AbstractDigestSpanEncoder;
+import com.alipay.common.tracer.core.span.CommonSpanTags;
+import com.alipay.common.tracer.core.span.SofaTracerSpan;
+import com.alipay.common.tracer.core.utils.StringUtils;
+import com.alipay.sofa.tracer.plugins.dubbo.constants.AttachmentKeyConstants;
+import io.opentracing.tag.Tags;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author: guolei.sgl (guolei.sgl@antfin.com) 2019/2/26 4:19 PM
+ * @since:
+ **/
+public class DubboServerDigestJsonEncoder extends AbstractDigestSpanEncoder {
+
+    @Override
+    public String encode(SofaTracerSpan sofaTracerSpan) throws IOException {
+        JsonStringBuilder data = new JsonStringBuilder();
+        //span end time
+        data.appendBegin(CommonSpanTags.TIME, Timestamp.format(sofaTracerSpan.getEndTime()));
+        Map<String, String> tagStr = sofaTracerSpan.getTagsWithStr();
+        Map<String, Number> tagNum = sofaTracerSpan.getTagsWithNumber();
+        SofaTracerSpanContext context = sofaTracerSpan.getSofaTracerSpanContext();
+        //TraceId
+        data.append(CommonSpanTags.TRACE_ID, context.getTraceId());
+        //SpanId
+        data.append(CommonSpanTags.SPAN_ID, context.getSpanId());
+        //Span Type
+        data.append(Tags.SPAN_KIND.getKey(), tagStr.get(Tags.SPAN_KIND.getKey()));
+        //local appName
+        data.append(CommonSpanTags.LOCAL_APP, tagStr.get(CommonSpanTags.LOCAL_APP));
+        //serviceName
+        data.append(CommonSpanTags.SERVICE, tagStr.get(CommonSpanTags.SERVICE));
+        //method
+        data.append(CommonSpanTags.METHOD, tagStr.get(CommonSpanTags.METHOD));
+        //local ip
+        data.append(CommonSpanTags.LOCAL_HOST, tagStr.get(CommonSpanTags.LOCAL_HOST));
+        //local port
+        data.append(CommonSpanTags.LOCAL_PORT, tagStr.get(CommonSpanTags.LOCAL_PORT));
+        //protocol
+        data.append(CommonSpanTags.PROTOCOL, tagStr.get(CommonSpanTags.PROTOCOL));
+
+        long serializeTime = getTime(tagNum.get(AttachmentKeyConstants.SERVER_SERIALIZE_TIME));
+        data.append(CommonSpanTags.SERVER_SERIALIZE_TIME, serializeTime);
+        long deserializeTime = getTime(tagNum.get(AttachmentKeyConstants.SERVER_DESERIALIZE_TIME));
+        data.append(CommonSpanTags.SERVER_DESERIALIZE_TIME, deserializeTime);
+
+        Number reqSizeNum = tagNum.get(AttachmentKeyConstants.SERVER_DESERIALIZE_SIZE);
+        data.append(CommonSpanTags.REQ_SIZE, reqSizeNum == null ? 0 : reqSizeNum.longValue());
+        Number respSizeNum = tagNum.get(AttachmentKeyConstants.SERVER_SERIALIZE_SIZE);
+        data.append(CommonSpanTags.RESP_SIZE, respSizeNum == null ? 0 : respSizeNum.longValue());
+
+        //Http status code
+        data.append(CommonSpanTags.RESULT_CODE, tagStr.get(CommonSpanTags.RESULT_CODE));
+        //error message
+        if (StringUtils.isNotBlank(tagStr.get(Tags.ERROR.getKey()))) {
+            data.append(Tags.ERROR.getKey(), tagStr.get(Tags.ERROR.getKey()));
+        }
+        //thread name
+        data.append(CommonSpanTags.CURRENT_THREAD_NAME,
+            tagStr.get(CommonSpanTags.CURRENT_THREAD_NAME));
+        //time-consuming ms
+        data.append(CommonSpanTags.TIME_COST_MILLISECONDS,
+            (sofaTracerSpan.getEndTime() - sofaTracerSpan.getStartTime()));
+        this.appendBaggage(data, context);
+        return data.toString();
+    }
+
+    private long getTime(Number number) {
+        if (number != null) {
+            return number.longValue();
+        }
+        return 0;
+    }
+
+    private void appendBaggage(JsonStringBuilder jsonStringBuilder,
+                               SofaTracerSpanContext sofaTracerSpanContext) {
+        //baggage
+        jsonStringBuilder.appendEnd(CommonSpanTags.BAGGAGE,
+            baggageSerialized(sofaTracerSpanContext));
+    }
+}
