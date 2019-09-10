41a168e2c0e088f7a547f2d6d78d83742561a224
卫恒 <guolei.sgl@antfin.com>
2019-09-10 16:36:35 +0800

    fix resettemplate invoke

--- sofa-tracer-plugins/sofa-tracer-resttmplate-plugin/src/main/java/com/sofa/alipay/tracer/plugins/rest/interceptor/AsyncRestTemplateRequestInterceptor.java
+++ sofa-tracer-plugins/sofa-tracer-resttmplate-plugin/src/main/java/com/sofa/alipay/tracer/plugins/rest/interceptor/AsyncRestTemplateRequestInterceptor.java
@@ -18,6 +18,7 @@ package com.sofa.alipay.tracer.plugins.rest.interceptor;
 
 import com.alipay.common.tracer.core.SofaTracer;
 import com.alipay.common.tracer.core.configuration.SofaTracerConfiguration;
+import com.alipay.common.tracer.core.holder.SofaTraceContextHolder;
 import com.alipay.common.tracer.core.registry.ExtendFormat;
 import com.alipay.common.tracer.core.span.CommonSpanTags;
 import com.alipay.common.tracer.core.span.SofaTracerSpan;
@@ -53,14 +54,28 @@ public class AsyncRestTemplateRequestInterceptor implements AsyncClientHttpReque
                                                                                                     throws IOException {
         SofaTracerSpan sofaTracerSpan = restTemplateTracer.clientSend(request.getMethod().name());
         appendRestTemplateRequestSpanTags(request, sofaTracerSpan);
+        Exception exception = null;
         try {
             ListenableFuture<ClientHttpResponse> result = execution.executeAsync(request, body);
             result.addCallback(new SofaTraceListenableFutureCallback(restTemplateTracer,
                 sofaTracerSpan));
             return result;
         } catch (IOException e) {
-            restTemplateTracer.clientReceiveTagFinish(sofaTracerSpan, String.valueOf(500));
+            exception = e;
             throw e;
+        } finally {
+            // when error , clear tl soon
+            if (exception != null) {
+                restTemplateTracer.clientReceive(String.valueOf(500));
+            } else {
+                // clear current
+                SofaTraceContextHolder.getSofaTraceContext().pop();
+                if (sofaTracerSpan != null && sofaTracerSpan.getParentSofaTracerSpan() != null) {
+                    // reset parent
+                    SofaTraceContextHolder.getSofaTraceContext().push(
+                        sofaTracerSpan.getParentSofaTracerSpan());
+                }
+            }
         }
     }
 
