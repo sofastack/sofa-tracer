074097aed96559f3c17d0d1a144b9c258c3de906
卫恒 <guolei.sgl@antfin.com>
2019-09-11 11:12:43 +0800

    fix test case

--- tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/resttemplate/RestTemplateTest.java
+++ tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/resttemplate/RestTemplateTest.java
@@ -21,6 +21,7 @@ import com.alipay.common.tracer.core.configuration.SofaTracerConfiguration;
 import com.alipay.sofa.tracer.boot.TestUtil;
 import com.alipay.sofa.tracer.boot.base.SpringBootWebApplication;
 import com.sofa.alipay.tracer.plugins.rest.RestTemplateLogEnum;
+import com.sofa.alipay.tracer.plugins.rest.SofaTracerRestTemplateBuilder;
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -28,7 +29,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -40,8 +40,6 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -55,9 +53,6 @@ import static org.junit.Assert.assertTrue;
 @TestPropertySource(locations = "classpath:application-non-json.properties")
 public class RestTemplateTest {
     protected static String logDirectoryPath = TracerLogRootDaemon.LOG_FILE_DIR;
-
-    @Autowired
-    protected RestTemplate  restTemplate;
 
     @BeforeClass
     public static void beforeClass() throws IOException {
@@ -79,7 +74,7 @@ public class RestTemplateTest {
      */
     @Test
     public void testRestTemplate404() throws Exception {
-        assertNotNull(restTemplate);
+        RestTemplate restTemplate = SofaTracerRestTemplateBuilder.buildRestTemplate();
         String restUrl = "http://localhost:8888/greeting";
         try {
             restTemplate.getForEntity(restUrl, String.class);
