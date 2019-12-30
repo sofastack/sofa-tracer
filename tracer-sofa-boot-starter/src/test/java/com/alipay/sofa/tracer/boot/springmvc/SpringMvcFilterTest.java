e94e7065ba136f0fa4e1e17d47cc44156be9f6cd
guolei.sgl <guolei.sgl@antfin.com>
2019-12-30 11:57:51 +0800

    fix test case

--- tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/springmvc/SpringMvcFilterTest.java
+++ tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/springmvc/SpringMvcFilterTest.java
@@ -16,17 +16,33 @@
  */
 package com.alipay.sofa.tracer.boot.springmvc;
 
+import com.alipay.common.tracer.core.appender.TracerLogRootDaemon;
+import com.alipay.common.tracer.core.configuration.SofaTracerConfiguration;
 import com.alipay.sofa.tracer.boot.TestUtil;
 import com.alipay.sofa.tracer.boot.base.AbstractTestBase;
+import com.alipay.sofa.tracer.boot.base.SpringBootWebApplication;
 import com.alipay.sofa.tracer.boot.base.controller.SampleRestController;
 import com.alipay.sofa.tracer.plugins.springmvc.SpringMvcLogEnum;
 import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -38,11 +54,40 @@ import static org.junit.Assert.assertTrue;
  * @author yangguanchao
  * @since 2018/05/01
  */
-@ActiveProfiles("non-json")
-public class SpringMvcFilterTest extends AbstractTestBase {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringBootWebApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-non-json.properties")
+public class SpringMvcFilterTest {
+
+    protected static String    logDirectoryPath = TracerLogRootDaemon.LOG_FILE_DIR;
+
+    @LocalServerPort
+    private int                definedPort;
+
+    @Autowired
+    protected TestRestTemplate testRestTemplate;
+
+    protected String           urlHttpPrefix;
+
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+        cleanLogDirectory();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        urlHttpPrefix = "http://localhost:" + definedPort;
+        AbstractTestBase.reflectSpringMVCClear();
+    }
 
     @Value("${spring.application.name}")
     private String appName;
+
+    @Before
+    public void before() throws Exception {
+        // wait for resources clean
+        TestUtil.waitForAsyncLog();
+    }
 
     @Test
     public void testSofaRestGet() throws Exception {
@@ -65,5 +110,32 @@ public class SpringMvcFilterTest extends AbstractTestBase {
 
         String logAppName = contents.get(0).split(",")[1];
         assertEquals(appName, logAppName);
+    }
+
+    /**
+     * clear directory
+     *
+     * @throws java.io.IOException
+     */
+    public static void cleanLogDirectory() throws IOException {
+        File file = new File(logDirectoryPath);
+        if (file.exists()) {
+            FileUtils.cleanDirectory(file);
+        }
+    }
+
+    protected static File customFileLog(String fileName) {
+        return new File(logDirectoryPath + File.separator + fileName);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        clearTracerProperties();
+    }
+
+    private static void clearTracerProperties() throws Exception {
+        Field propertiesField = SofaTracerConfiguration.class.getDeclaredField("properties");
+        propertiesField.setAccessible(true);
+        propertiesField.set(null, new ConcurrentHashMap<>());
     }
 }
