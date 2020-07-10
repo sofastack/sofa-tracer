52712127db5409fd53c8ade68cf63e4e3fe58cdb
chenchen <chenmudu@gmail.com>
2020-07-10 22:12:12 +0800

    Fixes gh-369

--- tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/datasource/DataSourceBFPPTest.java
+++ tracer-sofa-boot-starter/src/test/java/com/alipay/sofa/tracer/boot/datasource/DataSourceBFPPTest.java
@@ -22,15 +22,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.security.InvalidParameterException;
+import java.util.List;
 
 /**
  * @author qilong.zql
+ * @author chenchen
  * @since 2.2.0
  */
 public class DataSourceBFPPTest {
 
     private String jdbcUrl   = "jdbc:oracle:thin:@localhost:1521:orcl";
     private String jdbcUrl2  = "jdbc:oracle:thin:@//localhost:1521/orcl.city.com";
+    private String jdbcUrl1  = "jdbc:oracle:thin:@(DESCRIPTION=(FAILOVER=on)(ADDRESS=(PROTOCOL=tcp)(HOST=tns1-svr)(PORT=1521))(ADDRESS=(PROTOCOL=tcp)(HOST=tns2-svr)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=tnsServiceName)))";
+    private String jdbcUrl3  = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=tns3-svr)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=tns3ServiceName))";
+    private String jdbcUrl12 = "jdbc@:oracle:thin:";
+
     private String jdbcUrl4  = "jdbc:mysql://127.0.0.1:3306/imooc?useUnicode=true&amp;characterEncoding=utf-8";
     private String jdbcUrl5  = "jdbc:mysql://127.0.0.1:3306/dataBase";
     private String jdbcUrl9  = "jdbc@:mysql://127.0.0.1:3306/dataBase";
@@ -46,6 +52,8 @@ public class DataSourceBFPPTest {
     public void testDbType() {
         Assert.assertTrue("oracle".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl)));
         Assert.assertTrue("oracle".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl2)));
+        Assert.assertTrue("oracle".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl1)));
+        Assert.assertTrue("oracle".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl3)));
         Assert.assertTrue("mysql".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl4)));
         Assert.assertTrue("mysql".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl5)));
         Assert.assertTrue("sqlserver".equals(DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl6)));
@@ -79,12 +87,21 @@ public class DataSourceBFPPTest {
             error = true;
         }
         Assert.assertTrue(error);
+        error = false;
+        try {
+            DataSourceUtils.resolveDbTypeFromUrl(jdbcUrl12);
+        } catch (InvalidParameterException ex) {
+            error = true;
+        }
+        Assert.assertTrue(error);
     }
 
     @Test
     public void testDataBase() {
         Assert.assertTrue("orcl".equals(DataSourceUtils.resolveDatabaseFromUrl(jdbcUrl)));
         Assert.assertTrue("orcl.city.com".equals(DataSourceUtils.resolveDatabaseFromUrl(jdbcUrl2)));
+        Assert.assertTrue("tnsServiceName".equals(DataSourceUtils.resolveDatabaseFromUrl(jdbcUrl1)));
+        Assert.assertTrue("tns3ServiceName".equals(DataSourceUtils.resolveDatabaseFromUrl(jdbcUrl3)));
 
         Assert.assertTrue("imooc".equals(DataSourceUtils.resolveDatabaseFromUrl(jdbcUrl4)));
         Assert.assertTrue("dataBase".equals(DataSourceUtils.resolveDatabaseFromUrl(jdbcUrl5)));
@@ -127,6 +144,19 @@ public class DataSourceBFPPTest {
         endpoint = DataSourceUtils.getEndpointFromConnectionURL(jdbcUrl7);
         Assert.assertEquals("localhost", endpoint.getHost());
         Assert.assertEquals(1433, endpoint.getPort());
+    }
+
+    @Test
+    public void testEndpoints() {
+        List<Endpoint> endpoints;
+        endpoints = DataSourceUtils.getEndpointsFromConnectionURL(jdbcUrl1);
+        Assert.assertEquals(2, endpoints.size());
+        Assert.assertEquals("tns1-svr:1521", endpoints.get(0).getEndpoint());
+        Assert.assertEquals("tns2-svr:1521", endpoints.get(1).getEndpoint());
+
+        endpoints = DataSourceUtils.getEndpointsFromConnectionURL(jdbcUrl3);
+        Assert.assertEquals(1, endpoints.size());
+        Assert.assertEquals("tns3-svr:1521", endpoints.get(0).getEndpoint());
     }
 
 }
